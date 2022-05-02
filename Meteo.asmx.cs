2faf12c3bcf1db2c3d148037d8f0ee56dd5923b7
YOUSSEF_YL <YOUSSEF.BACHAR7@GMAIL.COM>
2022-05-02 02:33:45 +0000

    WebService

--- Meteo.asmx.cs
+++ Meteo.asmx.cs
@@ -14,7 +14,7 @@ namespace WEB_SERV
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [System.ComponentModel.ToolboxItem(false)]
     // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
-    // [System.Web.Script.Services.ScriptService]
+     [System.Web.Script.Services.ScriptService]
     public class Meteo : System.Web.Services.WebService
     {
 
